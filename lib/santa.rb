5624dea3b0fe53c1a05aa2352889cfd41e54b0d7
Sam Gibson <sam@ifdown.net>
2012-03-11 15:28:27 +0530

    New publishing API support

--- lib/santa.rb
+++ lib/santa.rb
@@ -2,22 +2,24 @@ require File.expand_path 'magnet', File.dirname(__FILE__)
 
 require 'httparty'
 require 'json'
+require 'uri'
 
 class Santa
   include HTTParty
 
-  def initialize(uri)
+  def initialize(user, uri)
+    @user = user
     @uri = uri
   end
 
   def get &block
-    response = self.class.get(@uri)
+    response = self.class.get(URI::join(@uri, "#{@user}/publications"))
     torrents = JSON.parse(response.body) \
       .map { |t| Magnet.new t['ih'], t['dn'], t['tr'] }
     block.call torrents unless torrents.empty?
   end
 
-  def publish magnets
-    self.class.put(@uri, body: magnets.to_json)
+  def publish recipient, magnets
+    self.class.put(URI::join(@uri, "#{@user}/publications/#{recipient}", body: magnets.to_json)
   end
 end
