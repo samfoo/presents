5836add57c4c8e36ea789f2a276c3ecfbdd2c573
Sam Gibson <sam@ifdown.net>
2012-03-31 12:02:48 +0530

    Move torrents to config

--- config.ru
+++ config.ru
@@ -1,3 +1,15 @@
 require './presents'
 
+set :username, 'dontcare'
+set :directory, 'foo'
+set :rpc_port, 5555
+
+raise "base dir must exists" unless Dir.exist? settings.directory
+
+state_directory = File.join settings.directory, '.presents'
+Dir.mkdir state_directory unless Dir.exist? state_directory
+transmission_directory = File.join state_directory, 'transmission'
+
+Torrents = Transmission.new settings.rpc_port, transmission_directory, settings.directory
+
 run Rack::Cascade.new [UIApp, PresentsApp]
--- presents.rb
+++ presents.rb
@@ -4,10 +4,6 @@ require 'coffee-script'
 require './lib/santa'
 require './lib/transmission'
 
-set :username, 'dontcare'
-set :directory, 'foo'
-set :rpc_port, 5555
-
 class UIApp < Sinatra::Application
   require 'haml'
 
@@ -26,25 +22,18 @@ end
 
 class PresentsApp < Sinatra::Application
   def initialize
-    raise "base dir must exists" unless Dir.exist? settings.directory
-
-    state_directory = File.join settings.directory, '.presents'
-    Dir.mkdir state_directory unless Dir.exist? state_directory
-    transmission_directory = File.join state_directory, 'transmission'
-
-    @transmission = Transmission.new settings.rpc_port, transmission_directory, settings.directory
-    @santa = Santa.new settings.username, settings.server
+    @torrents = Torrents
   end
 
   get '/:recipient/presents' do
     @santa.get do |magnets|
-      magnets.each { |m| @transmission.add m }
+      magnets.each { |m| @torrents.add m }
     end
   end
 
   put '/:recipient/presents' do
     path = params['path']
-    magnet = @transmission.seed path
+    magnet = @torrents.seed path
     @santa.publish params['recipient'], magnet
 
     content_type 'application/json'
